f2f32d0bf9396bc0cfcdace0959432cb3926cfa7
Ashraf Zaman <ashraf.turzo@gmail.com>
2017-10-05 18:00:09 +0200

    Adjust for smaller displays and add favicon

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -25,7 +25,7 @@ export default class IndecisionApp extends React.Component {
     };
     handleAddOption = (option) => {
         if (!option) {
-            return "Come on, don't give me an empty thought!";
+            return "Come on, don't give me blanks!";
         } else if (this.state.options.indexOf(option) > -1) {
             return "This option already exists";
         }
