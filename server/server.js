3c3ea7a5e15952ea9203ff95878c36651914ca7c
Ashraf Zaman <ashraf.turzo@gmail.com>
2017-10-05 16:13:51 +0200

    Fix an issue in server.js that caused heroku to crash

--- server/server.js
+++ server/server.js
@@ -11,5 +11,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Epress server is up on port ' + PORT);
+    console.log('Express server is up on port ' + port);
 });
